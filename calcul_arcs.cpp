d79d883a069429114d3d19020ebddb8d5ebb77b5
anaelle444 <anaellejoo.7@gmail.com>
2025-11-20 10:56:04 +0100

    comments

--- calcul_arcs.cpp
+++ calcul_arcs.cpp
@@ -25,24 +25,25 @@ int rechercher(const string& kmer, const vector<string>& L) {
 }
 
 // CalculArcs
-// Entrée : L : liste de k-mers (ordre alphabétique), k : entier
+// Entrée : L : liste triée de k-mers (ordre alphabétique), k : entier
 // Sortie : A : liste des arcs (paires d'indices des k-mers)
+
 vector<pair<int, int>> calculArcs(const vector<string>& L, int k) {
-    vector<pair<int, int>> A;  // Liste des arcs
-    char bases[] = {'A', 'C', 'G', 'T'};
+    vector<pair<int, int>> A;  // A <- liste vide
+    char bases[] = {'A', 'C', 'G', 'T'}; //alphabet
     
     // Pour chaque k-mer dans L
-    for (size_t i = 0; i < L.size(); i++) {
-        // Récupérer le suffixe de longueur k-1 du k-mer courant
-        string suffixe = L[i].substr(1, k - 1);
+    for (size_t i = 0; i < L.size(); i++) { //pour i de 0 a |L|-1, i : indice du kmer courant
+
+        std::string suffixe = L[i].substr(1, k - 1); // récupérer (substring) le suffixe longueur k-1 du k-mer courant (lomgueur du chevauchement)
         
         // Tester chaque base pour voir si elle forme un k-mer existant
-        for (char X : bases) {
+        for (char X : bases) { // pour chaque base
             // Construire le k-mer suivant potentiel
-            string kmer_suivant = suffixe + X;
+            string kmer_suivant = suffixe + X; // suffixe + 'X' pour voir si c'est le prefixe d'un kmer dans L
             
             // Chercher si ce k-mer existe dans L
-            int j = rechercher(kmer_suivant, L);
+            int j = rechercher(kmer_suivant, L); //retourne l'indice du kmer chevauchant s'il exitse
             
             // Si le k-mer existe, ajouter l'arc
             if (j != -1) {
